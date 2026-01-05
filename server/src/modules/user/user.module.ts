9cf010bc4468eda876bac7dd954b321dcd17266e
mzavidiy <mzavidiy@dbi.ru>
2026-01-05 14:45:29 +0300

    fixs server

--- server/src/modules/user/user.module.ts
+++ server/src/modules/user/user.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 
 import { UserService } from './services/user.service';
 import { UserController } from './controllers/user.controller';
-import { SmsService } from '../sms/services/sms.service';
+import { SmsModule } from '../sms/sms.module';
 
 @Module({
+    imports: [SmsModule],
     controllers: [UserController],
-    providers: [UserService, SmsService],
+    providers: [UserService],
     exports: [UserService],
 })
 export class UserModule {}
